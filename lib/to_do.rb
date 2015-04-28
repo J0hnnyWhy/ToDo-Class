bea1d7f7f4a691072971b37cfbde0152d11f74ce
John Young & Paul Morin <student@epicodus.com>
2015-04-28 11:00:58 -0700

    It will describe our To_Do class and pass the first rspec.

---
+++ lib/to_do.rb
@@ -0,0 +1,9 @@
+class ToDo
+  define_method(:initialize) do |description|
+    @description = description
+  end
+
+  define_method(:description) do
+    @description
+  end    
+end
---
+++ spec/to_do_spec.rb
@@ -0,0 +1,16 @@
+require('rspec')
+require('to_do')
+
+describe(ToDo) do
+  describe("description") do
+    it("lets you give a description") do
+      test_todo = ToDo.new("scrub the zebra")
+      expect(test_todo.description()).to(eq("scrub the zebra"))
+    end
+  end
+
+  describe(".all") do
+    it("is empty at first") do
+      expect(ToDo.all()).to(eq([]))
+  end
+end
